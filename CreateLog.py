23a60893db3d2e56850bd1df8115a7452105f1cd
NA33AT0R <Gabrielllarsen@outlook.com>
2025-10-22 09:39:27 +0200

    added XES function

--- CreateLog.py
+++ CreateLog.py
@@ -1,5 +1,36 @@
 #field is a string that explains the activity we want to look at (for example last_place_name or is_alive)
 # this will look for a change in values, if a player is alive in 1 tick, and dead in another this will be noted in a dictionary
+import pm4py
+from pm4py.objects.log.obj import EventLog, Trace, Event
+from datetime import datetime, timedelta
+import random
+import xml.etree.cElementTree as ET
+
+def creatXes(dict_log):
+    event_log = EventLog()
+    # implement way to split into rounds, where each round is a trace, currently everything is just one large trace
+    for _ in range(1):
+        trace = Trace()
+        for tick in dict_log:
+            for action in range(0,len(dict_log[tick])):
+
+                event = Event()
+                event['user:playername'] = dict_log[tick][action][0]
+                event['concept:activity'] = dict_log[tick][action][1]
+                event['time:tick'] = tick
+                event['custom:change'] = dict_log[tick][action][2]  
+         
+                trace.append(event)
+     
+        event_log.append(trace)
+
+    # Specify the path to your output XES file
+    output_xes_file = 'csgo_EventLog.xes'
+     
+    # Write the event log to the XES file
+    pm4py.write_xes(event_log, output_xes_file)
+
+
 def getActivityFromField(field:str, df):
 
     field_dict = dict()
@@ -39,6 +70,9 @@ def getActivityLog(activities:list[str], df):
                     log_dict[tick].append((player, field, field_value))
 
     return log_dict
+
+
+
 
 
 # print(getActivityLog(["is_alive", "last_place_name"], df))
