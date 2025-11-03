d0ea5fbeaca6faff0a43758f81ebfc8f34b42c02
s221516 <s221516@dtu.dk>
2025-11-03 11:22:41 +0100

    Added team names to activties

--- CreateLog.py
+++ CreateLog.py
@@ -1,85 +1,110 @@
 from demoparser2 import DemoParser
+
+# ticks = parser.parse_ticks(["tick", "name", "is_alive", "team_name","bomb_exploded"])
+
+
 parser = DemoParser("heroic-vs-3dmax-m1-dust2.dem")
-ticks = parser.parse_ticks(["tick", "name", "is_alive", "team_name","bomb_exploded"])
-
-list_of_activities = ["name", "is_alive", "last_place_name"]
-#TODO remove "name"
-
-# print(ticks)
-
-# print(parser.list_game_events())
-
-#print(list(parser.columns.values))
-#print(list(ticks))
-
-# print(parser.parse_event("round_start"))
-# print(parser.parse_event("round_end"))
+round_start = list(parser.parse_event("round_start")["tick"])
+round_end = list(parser.parse_event("round_end")["tick"])
 
 
+class Activity:
+    def __init__(self, field, isStartTickImportant):
+        self.isStartTickImportant = isStartTickImportant #is the state of the field obvious in the first tick?
+        self.field = field
+
+    def isStartTickImportant(self):
+        return self.isStartTickImportant
+    
+    def getField(self):
+        return self.field
+    
+
+startTickNotImportant = ["is_alive"]
+startTickImportant = ["last_place_name"]
+activities_for_demoparser = startTickImportant + startTickNotImportant
 
 
+activities = []
+for field in startTickImportant:
+    activities.append(Activity(field, True))
 
-def getActivityFromField(field:str, df):
-
+for field in startTickNotImportant:
+    activities.append(Activity(field, False))
+    
+def getActivityFromField(activity:Activity, df) -> dict:
+    """
+    Parameter1: Is the activtiy that we want to track in the df
+    Parameter2: df
+    return: dictionary of players and their activites in a round.
+    """
     field_dict = dict()
-
-    for _, row in df.iterrows():
-        name = row["name"]
-        field_value = row[field]
-        # We found some anomalies, where no zone was given, we are disregarding these
-        if field_value == "":
-            continue 
+    for _, row in df.iterrows(): 
+        team_name = row["team_name"] #CT or T
+        player_name = row["name"]
+        field_name = activity.getField()
+        field_value = row[field_name]
+        activity_label = team_name + "_" + str(field_value)
         tick = row["tick"]
 
-        if name not in field_dict:
-            field_dict[name] = [(field, field_value, tick)]
-
+        if player_name not in field_dict:
+                field_dict[player_name] = [(activity_label, tick, field_name)]
         else:
-            _, previous_field_value,_ = field_dict[name][-1]
-            if previous_field_value != field_value:
-                field_dict[name].append((field, field_value,tick))
-
-
+            previous_activity_label, _, _ = field_dict[player_name][-1]
+            if previous_activity_label != activity_label:
+                field_dict[player_name].append((activity_label, tick, field_name))
     return field_dict
 
-# this function itterates over the list of activities given, for example ["is_alive", "last_place_name"]. And then makes a new dictionary
-# where each 
-def getActivityLog(activities:list[str], df):
+
+def getActivityLog(activities:list[Activity], df) -> dict:
+    """
+    Parameter1: List of activities that we want to track in the DF
+    Parameter2: DF
+    Return dictionary of ticks and activites - sorted by ticks.
+    """
     log_dict = dict()
     for activity in activities:
-        field_dict = getActivityFromField(activity, df)
+        field_dict = getActivityFromField(activity, df)    
+        
         for player in field_dict:
             player_activities = field_dict[player]
+            
             for activity in player_activities:
-                field, field_value, tick = activity
+                activity_label, tick, field_name = activity
                 if tick not in log_dict:
-                    log_dict[tick] = [(player, field, field_value)]
+                    log_dict[tick] = [(activity_label, field_name, player)] #Remove player if unecessary
                 else:
-                    log_dict[tick].append((player, field, field_value))
-
+                    log_dict[tick].append((activity_label, field_name, player)) 
     return dict(sorted(log_dict.items()))
 
 
 
-# Returns a list of dictionaries, where each dictionary is the activity log for a round
-def getListOfActivitiesPerRound():
-    round_start = list(parser.parse_event("round_start")["tick"])
-    round_end = list(parser.parse_event("round_end")["tick"])
 
+def getListOfActivitiesPerRound() -> list[dict]:
+    """
+    Return list of dictionaries with activites sorted by ticks for each round.
+    """
     list_of_round_interval = []
     for i in range(len(round_start)):
         list_of_round_interval.append((round_start[i],round_end[i+1]))
 
-    list_of_dict = []    
+    list_of_dictForEachRound = []   
+    print(list_of_round_interval) 
     for interval in list_of_round_interval:
         start_tick = interval[0]
         end_tick = interval[1]
         
         list_of_ticks_for_round_i = list(range(start_tick,end_tick+1))
-        round_i_df = parser.parse_ticks(list_of_activities, ticks = list_of_ticks_for_round_i)
-        activity_log_round_i = getActivityLog(list_of_activities, round_i_df)
-        list_of_dict.append(activity_log_round_i)
+        print(list_of_ticks_for_round_i[0])
+        print(list_of_ticks_for_round_i[-1])
+        round_i_df = parser.parse_ticks(activities_for_demoparser + ["team_name"], ticks = list_of_ticks_for_round_i)
+        activity_log_round_i = getActivityLog(activities, round_i_df)
+        list_of_dictForEachRound.append(activity_log_round_i)
+        
+        break
 
-    return list_of_dict
+    return list_of_dictForEachRound
 
-
+print("housetour", str(True))
+log = getListOfActivitiesPerRound()
+print(log)
