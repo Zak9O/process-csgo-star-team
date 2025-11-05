a3ce80a55d6d04ff9ad42fc6ce9bb0e92f165512
Dreambot <william.bendix@live.dk>
2025-11-05 11:35:24 +0100

    Added teamname in activity label Made optimized version in gigity for fast logs

--- CreateLog.py
+++ CreateLog.py
@@ -1,44 +1,116 @@
-#field is a string that explains the activity we want to look at (for example last_place_name or is_alive)
-# this will look for a change in values, if a player is alive in 1 tick, and dead in another this will be noted in a dictionary
-def getActivityFromField(field:str, df):
+import time
+from demoparser2 import DemoParser
 
+start = time.time()
+# ticks = parser.parse_ticks(["tick", "name", "is_alive", "team_name","bomb_exploded"])
+
+
+parser = DemoParser("heroic-vs-3dmax-m1-dust2.dem")
+round_start = list(parser.parse_event("round_start")["tick"])
+round_end = list(parser.parse_event("round_end")["tick"])
+
+
+class Activity:
+    def __init__(self, field, isStartTickImportant):
+        self.isStartTickImportant = isStartTickImportant #is the state of the field obvious in the first tick?
+        self.field = field
+
+    def getField(self):
+        return self.field
+    
+
+startTickNotImportant = ["is_alive"]
+startTickImportant = ["last_place_name"]
+activities_for_demoparser = startTickImportant + startTickNotImportant
+
+
+activities = []
+for field in startTickImportant:
+    activities.append(Activity(field, True))
+
+for field in startTickNotImportant:
+    activities.append(Activity(field, False))
+    
+def getActivityFromField(activity:Activity, df) -> dict:
+    """
+    Parameter1: Is the activtiy that we want to track in the df
+    Parameter2: df
+    return: dictionary of players and their activites in a round.
+    """
     field_dict = dict()
-
-    for _, row in df.iterrows():
-        name = row["name"]
-        field_value = row[field]
-        # We found some anomalies, where no zone was given, we are disregarding these
-        if field_value == "":
-            continue 
+    for _, row in df.iterrows(): 
+        team_name = row["team_name"] #CT or T
+        player_name = row["name"]
+        field_name = activity.getField()
+        field_value = row[field_name]
+        activity_label = team_name + "_" + str(field_value)
         tick = row["tick"]
 
-        if name not in field_dict:
-            field_dict[name] = [(field, field_value, tick)]
-
+        if player_name not in field_dict:
+                field_dict[player_name] = [(activity_label, tick, field_name)]
         else:
-            _, previous_field_value,_ = field_dict[name][-1]
-            if previous_field_value != field_value:
-                field_dict[name].append((field, field_value,tick))
+            previous_activity_label, _, _ = field_dict[player_name][-1]
+            if previous_activity_label != activity_label:
+                field_dict[player_name].append((activity_label, tick, field_name))
 
-
+                
     return field_dict
 
-# this function itterates over the list of activities given, for example ["is_alive", "last_place_name"]. And then makes a new dictionary
-# where each 
-def getActivityLog(activities:list[str], df):
+
+
+def getActivityLog(activities:list[Activity], df) -> dict:
+    """
+    Parameter1: List of activities that we want to track in the DF
+    Parameter2: DF
+    Return dictionary of ticks and activites - sorted by ticks.
+    """
     log_dict = dict()
     for activity in activities:
         field_dict = getActivityFromField(activity, df)
+                        
         for player in field_dict:
-            player_activities = field_dict[player]
-            for activity in player_activities:
-                field, field_value, tick = activity
+            player_activities = field_dict[player]    
+            for activity_1 in player_activities:
+                activity_label, tick, field_name = activity_1
+                
+                if not activity.isStartTickImportant:
+                    if tick in round_start:
+                        continue
+                    
                 if tick not in log_dict:
-                    log_dict[tick] = [(player, field, field_value)]
+                    log_dict[tick] = [(activity_label, field_name, player)] #Remove player if unecessary
                 else:
-                    log_dict[tick].append((player, field, field_value))
+                    log_dict[tick].append((activity_label, field_name, player)) 
+    return dict(sorted(log_dict.items()))
 
-    return log_dict
+def getListOfActivitiesPerRound() -> list[dict]:
+    """
+    Return list of dictionaries with activites sorted by ticks for each round.
+    """
+    list_of_round_interval = []
+    for i in range(len(round_start)):
+        list_of_round_interval.append((round_start[i],round_end[i+1]))
 
+    list_of_dictForEachRound = []   
+    counter = 0
+    for interval in list_of_round_interval:
+        start_tick = interval[0]
+        end_tick = interval[1]
+        
+        list_of_ticks_for_round_i = list(range(start_tick,end_tick+1))
+        round_i_df = parser.parse_ticks(activities_for_demoparser + ["team_name"], ticks = list_of_ticks_for_round_i)
+        activity_log_round_i = getActivityLog(activities, round_i_df)
+        list_of_dictForEachRound.append(activity_log_round_i)
+            
+    return list_of_dictForEachRound
 
-# print(getActivityLog(["is_alive", "last_place_name"], df))
+log = getListOfActivitiesPerRound()
+
+for item in log:
+
+    for i, j in item.items():
+        print(i, j)
+
+end = time.time()
+time_ = end - start
+print(time_)
---
+++ OptimizedLog.py
@@ -0,0 +1,82 @@
+from demoparser2 import DemoParser
+import pandas as pd
+import time
+
+start = time.time()
+
+parser = DemoParser("../heroic-vs-3dmax-m1-dust2.dem")
+
+round_start = parser.parse_event("round_start")["tick"].to_list()
+round_end = parser.parse_event("round_end")["tick"].to_list()
+
+class Activity:
+    def __init__(self, field, isStartTickImportant):
+        self.isStartTickImportant = isStartTickImportant
+        self.field = field
+    def getField(self):
+        return self.field
+
+startTickNotImportant = ["is_alive"]
+startTickImportant = ["last_place_name"]
+activities = [Activity(f, True) for f in startTickImportant] + [Activity(f, False) for f in startTickNotImportant]
+activities_for_demoparser = startTickImportant + startTickNotImportant
+
+
+print("Parsing all ticks once...")
+all_ticks_df = parser.parse_ticks(activities_for_demoparser + ["team_name", "name", "tick"])
+print("Done parsing ticks.")
+
+
+def getActivityFromField(activity: Activity, df: pd.DataFrame) -> dict:
+    field = activity.getField()
+    df = df.sort_values(["name", "tick"])
+
+    df["prev_value"] = df.groupby("name")[field].shift(1)
+    changes = df[df[field] != df["prev_value"]].dropna(subset=[field])
+
+    field_dict = (
+    changes.groupby("name")
+    .apply(lambda g: list(zip(
+        g["team_name"] + "_" + g[field].astype(str),
+        g["tick"],
+        [field] * len(g)
+    )))
+    .to_dict()
+    )
+
+    return field_dict
+
+
+def getActivityLog(activities: list[Activity], df: pd.DataFrame) -> dict:
+    log_dict = {}
+    for activity in activities:
+        field_dict = getActivityFromField(activity, df)
+        for player, activity_list in field_dict.items():
+            for activity_label, tick, field_name in activity_list:
+                if not activity.isStartTickImportant and tick in round_start:
+                    continue
+                log_dict.setdefault(tick, []).append((activity_label, field_name, player))
+
+    return dict(sorted(log_dict.items()))
+
+
+def getListOfActivitiesPerRound() -> list[dict]:
+    round_dicts = []
+    for i in range(len(round_start) - 1):
+        start_tick = round_start[i]
+        end_tick = round_end[i + 1]
+        round_df = all_ticks_df.query("@start_tick <= tick <= @end_tick")
+        activity_log = getActivityLog(activities, round_df)
+        round_dicts.append(activity_log)
+    return round_dicts
+
+
+log = getListOfActivitiesPerRound()
+
+for round_dict in log:
+    for tick, data in round_dict.items():
+        print(tick, data)
+
+end = time.time()
+time_ = end - start
+print(time_)
